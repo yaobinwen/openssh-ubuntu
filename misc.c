3e63c79f0594bdd79a820ddec3b085255ae78405
Martin Pitt <martin.pitt@ubuntu.com>
2006-02-20 14:47:35 +0000

    1:3.8.1p1-11ubuntu3.3 (patches unapplied)
    Imported using git-ubuntu import.

--- misc.c
+++ misc.c
@@ -309,10 +309,13 @@ addargs(arglist *args, char *fmt, ...)
 	va_list ap;
 	char buf[1024];
 	int nalloc;
+	int r;
 
 	va_start(ap, fmt);
-	vsnprintf(buf, sizeof(buf), fmt, ap);
+	r = vsnprintf(buf, sizeof(buf), fmt, ap);
 	va_end(ap);
+	if (r == -1 || r >= (int)sizeof(buf))
+		fatal("addargs: argument too long");
 
 	nalloc = args->nalloc;
 	if (args->list == NULL) {
@@ -325,4 +328,38 @@ addargs(arglist *args, char *fmt, ...)
 	args->nalloc = nalloc;
 	args->list[args->num++] = xstrdup(buf);
 	args->list[args->num] = NULL;
+}
+
+void
+replacearg(arglist *args, u_int which, char *fmt, ...)
+{
+	va_list ap;
+	char buf[1024];
+	int r;
+
+	va_start(ap, fmt);
+	r = vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	if (r == -1 || r > (int)sizeof(buf))
+		fatal("replacearg: argument too long");
+
+	if (which >= args->num)
+		fatal("replacearg: tried to replace invalid arg %d >= %d",
+		    which, args->num);
+	xfree(args->list[which]);
+	args->list[which] = xstrdup(buf);
+}
+
+void
+freeargs(arglist *args)
+{
+	u_int i;
+
+	if (args->list != NULL) {
+		for (i = 0; i < args->num; i++)
+			xfree(args->list[i]);
+		xfree(args->list);
+		args->nalloc = args->num = 0;
+		args->list = NULL;
+	}
 }
