5400794517c668d83a26d7b7010f0475a861396c
Sergio Durigan Junior <sergio.durigan@canonical.com>
2020-05-13 10:12:28 -0400

    upstream commit
    Gbp-Pq: CVE-2016-10708.patch.

--- kex.c
+++ kex.c
@@ -431,6 +431,8 @@ kex_input_newkeys(int type, u_int32_t seq, void *ctxt)
 	ssh_dispatch_set(ssh, SSH2_MSG_NEWKEYS, &kex_protocol_error);
 	if ((r = sshpkt_get_end(ssh)) != 0)
 		return r;
+	if ((r = ssh_set_newkeys(ssh, MODE_IN)) != 0)
+		return r;
 	kex->done = 1;
 	sshbuf_reset(kex->peer);
 	/* sshbuf_reset(kex->my); */
