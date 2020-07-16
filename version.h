b97e7f2c31f569f47885d4048fec1f31ad66fabd
Ubuntu Git Importer <usd-importer-do-not-mail@canonical.com>
2020-07-16 17:26:56 +0000

    New upstream version 7.1p1

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.73 2015/07/01 01:55:13 djm Exp $ */
+/* $OpenBSD: version.h,v 1.75 2015/08/21 03:45:26 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.9"
+#define SSH_VERSION	"OpenSSH_7.1"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
