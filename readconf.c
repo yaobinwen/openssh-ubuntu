27f5ec78d20d6aba3da79ca07bf8e7624ca1e59a
Nick Rosbrook <enr0n@ubuntu.com>
2023-11-13 12:47:29 -0500

    Accept obsolete ssh-vulnkey configuration options
    Gbp-Pq: ssh-vulnkey-compat.patch.

--- readconf.c
+++ readconf.c
@@ -197,6 +197,7 @@ static struct {
 	{ "fallbacktorsh", oDeprecated },
 	{ "globalknownhostsfile2", oDeprecated },
 	{ "rhostsauthentication", oDeprecated },
+	{ "useblacklistedkeys", oDeprecated },
 	{ "userknownhostsfile2", oDeprecated },
 	{ "useroaming", oDeprecated },
 	{ "usersh", oDeprecated },
