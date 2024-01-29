f3ffaf8e142bc8473c155a3c4b2d806af9a1363d
Miriam España Acebal <miriam.espana@canonical.com>
2024-01-29 11:16:31 +0100

    GSSAPI key exchange support
    Gbp-Pq: gssapi.patch.

--- sshkey.c
+++ sshkey.c
@@ -128,6 +128,9 @@ extern const struct sshkey_impl sshkey_dsa_cert_impl;
 extern const struct sshkey_impl sshkey_xmss_impl;
 extern const struct sshkey_impl sshkey_xmss_cert_impl;
 #endif
+#ifdef GSSAPI
+extern const struct sshkey_impl sshkey_null_impl;
+#endif /* GSSAPI */
 
 const struct sshkey_impl * const keyimpls[] = {
 	&sshkey_ed25519_impl,
@@ -165,6 +168,9 @@ const struct sshkey_impl * const keyimpls[] = {
 	&sshkey_xmss_impl,
 	&sshkey_xmss_cert_impl,
 #endif
+#ifdef GSSAPI
+	&sshkey_null_impl,
+#endif /* GSSAPI */
 	NULL
 };
 
@@ -320,7 +326,7 @@ sshkey_alg_list(int certs_only, int plain_only, int include_sigonly, char sep)
 
 	for (i = 0; keyimpls[i] != NULL; i++) {
 		impl = keyimpls[i];
-		if (impl->name == NULL)
+		if (impl->name == NULL || impl->type == KEY_NULL)
 			continue;
 		if (!include_sigonly && impl->sigonly)
 			continue;
